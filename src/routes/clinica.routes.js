69207aed5010f5dd1d0ff4a4765a7771e3eaf33b
TeranJ <teranj1902@gmail.com>
2024-06-14 21:35:54 -0500

    Integrando

--- src/routes/clinica.routes.js
+++ src/routes/clinica.routes.js
@@ -1,14 +1,18 @@
 const {Router} = require('express')
-const {getAllTickets, getSpecificTicket, addTicket, deleteTicket, modifyTicket} = require('../controllers/clinica.controller')
+const {getAllTickets, 
+    getSpecificTicket, 
+    addTicket, 
+    deleteTicket, 
+    modifyTicket} = require('../controllers/clinica.controller')
 const router = Router();
 
 router.get('/ticket', getAllTickets)
 
 router.get('/ticket/:id', getSpecificTicket)
 
-router.post('/tasks', addTicket)
+router.post('/add-ticket', addTicket)
 
-router.delete('/tasks', deleteTicket)
+router.delete('/delete-ticket', deleteTicket)
 
 router.put('/ticket/:id', modifyTicket)
 
